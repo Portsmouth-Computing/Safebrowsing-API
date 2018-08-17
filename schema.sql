280d307892116b262e8f89b5b89eb0e78923ca04
Lagicrus <Lagicrus@users.noreply.github.com>
2018-08-18 00:01:17 +0100

    Added table

--- schema.sql
+++ schema.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS url_storage (
 
     google_safe_browsing_result JSON
 );
+
+CREATE TABLE IF NOT EXISTS url_redirect_storage (
+   url TEXT PRIMARY KEY CHECK (char_length(url) <= 500),
+   amount_of_redirects BIGINT,
+   end_url TEXT,
+   accessed BIGINT DEFAULT 1
+);
